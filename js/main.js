985a9fadfe75d50f2852a6176c9437ed41eaab30
roilopezZ <64579967+roilopezZ@users.noreply.github.com>
2021-03-14 23:38:32 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,404 @@
+// ********************************** 1366 Screen Stting plus Minus btns ********************************** //
+
+// -------------- box Left -------------- // 
+var numberA = 1;
+var count1 = document.getElementById("numbers");
+
+function plus() {
+    numberA++;
+    count1.value = this.numberA;
+    // count2.value = this.numberB;
+}
+
+function minus() {
+    if (numberA > 1 ) {
+        numberA--;
+        count1.value= this.numberA;
+    }
+}
+// -------------- End box Left -------------- //
+
+
+// -------------- box right -------------- // 
+var numberB = 1;
+var count2 = document.getElementById("numbers2");
+function plus2() {
+    numberB++;
+    count2.value = this.numberB;
+}
+
+function minus2() {
+    if (numberB > 1 ) {
+        numberB--;
+        count2.value= this.numberB;
+    }
+}
+
+// -------------- End box right -------------- // 
+
+
+
+// -------------- box 3 -------------- // 
+var numberC = 1;
+var count3 = document.getElementById("numbers3");
+
+function plus3() {
+    numberC++;
+    count3.value = this.numberC;
+}
+
+function minus3() {
+    if (numberC > 1 ) {
+        numberC--;
+        count3.value= this.numberC;
+    }
+}
+// -------------- End box 3 -------------- // 
+
+
+
+
+
+// -------------- box 4 -------------- // 
+var numberD = 1;
+var count4 = document.getElementById("numbers4");
+function plus4() {
+    numberD++;
+    count4.value = this.numberD;
+}
+
+function minus4() {
+    if (numberD > 1 ) {
+        numberD--;
+        count4.value= this.numberD;
+    }
+}
+// -------------- End box 4 -------------- // 
+
+
+// -------------- box 5 -------------- // 
+var numberE = 1;
+var count5 = document.getElementById("numbers5");
+
+function plus5() {
+    numberE++;
+    count5.value = this.numberE;
+}
+
+function minus5() {
+    if (numberE > 1 ) {
+        numberE--;
+        count5.value= this.numberE;
+    }
+}
+// -------------- End box 5 -------------- // 
+
+// ********************************** End 1366 Screen Stting plus Minus btns ********************************** // 
+
+
+
+
+
+// ********************************** 768 Screen Stting plus Minus btns ********************************** // 
+
+// -------------- box 1 left -------------- // 
+
+var numberF = 1;
+var count6 = document.getElementById("numbers6");
+function plus6() {
+    numberF++;
+    count6.value = this.numberF;
+}
+
+function minus6() {
+    if (numberF > 1 ) {
+        numberF--;
+        count6.value= this.numberF;
+    }
+}
+// -------------- End box 1 left -------------- // 
+
+
+// -------------- box 2  Right -------------- // 
+
+var numberG = 1;
+var count7 = document.getElementById("numbers7");
+function plus7() {
+    numberG++;
+    count7.value = this.numberG;
+}
+
+function minus7() {
+    if (numberG > 1 ) {
+        numberG--;
+        count7.value= this.numberG;
+    }
+}
+// -------------- End box 2 Right -------------- // 
+
+
+
+// -------------- box 3 Left -------------- // 
+
+var numberH = 1;
+var count8 = document.getElementById("numbers8");
+function plus8() {
+    numberH++;
+    count8.value = this.numberH;
+}
+
+function minus8() {
+    if (numberH > 1 ) {
+        numberH--;
+        count8.value= this.numberH;
+    }
+}
+// -------------- End box 3 Left -------------- // 
+
+
+// -------------- box 4 Right -------------- // 
+
+var numberI = 1;
+var count9 = document.getElementById("numbers9");
+function plus9() {
+    numberI++;
+    count9.value = this.numberI;
+}
+
+function minus9() {
+    if (numberI > 1 ) {
+        numberI--;
+        count9.value= this.numberI;
+    }
+}
+// -------------- End box 4 Right -------------- // 
+
+
+// -------------- box 5 Left -------------- // 
+
+var numberJ = 1;
+var count10 = document.getElementById("numbers10");
+function plus10() {
+    numberJ++;
+    count10.value = this.numberJ;
+}
+
+function minus10() {
+    if (numberJ > 1 ) {
+        numberJ--;
+        count10.value= this.numberJ;
+    }
+}
+// -------------- End box 5 Left -------------- // 
+
+
+
+
+
+// -------------- box 6 Right -------------- // 
+
+var numberK = 1;
+var count11 = document.getElementById("numbers11");
+function plus11() {
+    numberK++;
+    count11.value = this.numberK;
+}
+
+function minus11() {
+    if (numberK > 1 ) {
+        numberK--;
+        count11.value= this.numberK;
+    }
+}
+// -------------- End box 6 Right -------------- // 
+
+
+
+// -------------- box 7 Left -------------- // 
+
+var numberL = 1;
+var count12 = document.getElementById("numbers12");
+function plus12() {
+    numberL++;
+    count12.value = this.numberL;
+}
+
+function minus12() {
+    if (numberL > 1 ) {
+        numberL--;
+        count12.value= this.numberL;
+    }
+}
+// -------------- End box 7 Left -------------- // 
+
+
+// -------------- box 8 Right -------------- // 
+
+var numberM = 1;
+var count13 = document.getElementById("numbers13");
+function plus13() {
+    numberM++;
+    count13.value = this.numberM;
+}
+
+function minus13() {
+    if (numberM > 1 ) {
+        numberM--;
+        count13.value= this.numberM;
+    }
+}
+// -------------- End box 8 Right -------------- // 
+
+
+
+// -------------- box 9 Left -------------- // 
+
+var numberN = 1;
+var count14 = document.getElementById("numbers14");
+function plus14() {
+    numberN++;
+    count14.value = this.numberN;
+}
+
+function minus14() {
+    if (numberN > 1 ) {
+        numberN--;
+        count14.value= this.numberN;
+    }
+}
+// -------------- End box 9 Left -------------- // 
+
+
+// -------------- box 10 Right -------------- // 
+
+var numberO = 1;
+var count15 = document.getElementById("numbers15");
+function plus15() {
+    numberO++;
+    count15.value = this.numberO;
+}
+
+function minus15() {
+    if (numberO > 1 ) {
+        numberO--;
+        count15.value= this.numberO;
+    }
+}
+// -------------- End box 10 Right -------------- // 
+
+
+
+// ********************************** End 768 Screen Stting plus Minus btns ********************************** // 
+
+
+
+// ********************************** 375 Screen Stting plus Minus btns ********************************** // 
+// -------------- box 1 Left -------------- // 
+
+var numberP = 1;
+var count16 = document.getElementById("numbers16");
+function plus16() {
+    numberP++;
+    count16.value = this.numberP;
+}
+
+function minus16() {
+    if (numberP > 1 ) {
+        numberP--;
+        count16.value= this.numberP;
+    }
+}
+// -------------- End box 1 Left -------------- //
+
+
+
+// -------------- box 2 Right -------------- // 
+
+var numberQ = 1;
+var count17 = document.getElementById("numbers17");
+function plus17() {
+    numberQ++;
+    count17.value = this.numberQ;
+}
+
+function minus17() {
+    if (numberQ > 1 ) {
+        numberQ--;
+        count17.value= this.numberQ;
+    }
+}
+// -------------- End box 2 Right -------------- //
+
+// -------------- box 3 Left -------------- // 
+
+var numberR = 1;
+var count18 = document.getElementById("numbers18");
+function plus18() {
+    numberR++;
+    count18.value = this.numberR;
+}
+
+function minus18() {
+    if (numberR > 1 ) {
+        numberR--;
+        count18.value= this.numberR;
+    }
+}
+// -------------- End box 2 Left -------------- //
+
+
+
+// -------------- box 4 Right -------------- // 
+
+var numberS = 1;
+var count19 = document.getElementById("numbers19");
+function plus19() {
+    numberS++;
+    count19.value = this.numberS;
+}
+
+function minus19() {
+    if (numberS > 1 ) {
+        numberS--;
+        count19.value= this.numberS;
+    }
+}
+// -------------- End box 4 Right -------------- //
+
+
+
+// -------------- box 5 Left -------------- // 
+
+var numberT = 1;
+var count20 = document.getElementById("numbers20");
+function plus20() {
+    numberT++;
+    count20.value = this.numberT;
+}
+
+function minus20() {
+    if (numberT > 1 ) {
+        numberT--;
+        count20.value= this.numberT;
+    }
+}
+// -------------- End box 5 Left -------------- //
+
+
+// -------------- box 6 Right -------------- // 
+
+var numberU = 1;
+var count21 = document.getElementById("numbers21");
+function plus21() {
+    numberU++;
+    count21.value = this.numberU;
+}
+
+function minus21() {
+    if (numberU > 1 ) {
+        numberU--;
+        count21.value= this.numberU;
+    }
+}
+// -------------- End box 6 Right -------------- //
+// **********************************  End 375 Screen Stting plus Minus btns ********************************** // 
